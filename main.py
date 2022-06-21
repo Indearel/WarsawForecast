d3ccc3a1f182e03c5047da1d3bacd36597cc151c
cyberindygos <cyberindygos@gmail.com>
2022-06-21 16:35:00 +0200

    Added current date + saving data to json.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
+import json
+from datetime import date
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'a') as fp:
+        json.dump(data, fp)
 
 def get_weather_descprition_and_temp():
     api_key = "token"
@@ -20,10 +27,26 @@ def get_weather_descprition_and_temp():
 
 def main():
     weather_dict = get_weather_descprition_and_temp()
+    today = date.today()
+    current_date = today.strftime("%d.%m.%Y")
+
     # Print the results
+    print("Today is:", current_date)
     print("Today`s forecast is",  weather_dict.get('description'))
     print("The minimum temperature is", weather_dict.get('temp_min'), "Celcius degrees.")
     print("The maximum temperature is", weather_dict.get('temp_max'), "Celcius degrees.")
     print("The pressure is", weather_dict.get('pressure'), "Hectopascals.")
+
+    path = "./"
+    filename = 'weather'
+
+    data = {}
+    data['Date'] = str(current_date)
+    data['Today`s forecast is '] = weather_dict.get('description')
+    data['The minimum temperature is '] = weather_dict.get('temp_min')
+    data['The maximum temperature is '] = weather_dict.get('temp_max')
+    data['The pressure is '] = weather_dict.get('pressure')
+
+    writeToJSONFile(path, filename, data)
 
 main()
