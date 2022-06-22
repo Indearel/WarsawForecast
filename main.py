2e55fc2ef73ffadcedf8b590975ec2d4cc1b18d5
cyberindygos <cyberindygos@gmail.com>
2022-06-22 22:07:17 +0200

    Added function that saves data into txt file. Formatting to be changed.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def writeToJSONFile(path, fileName, data):
         json.dump(data, fp)
 
 def get_weather_descprition_and_temp():
-    api_key = "token"
+    api_key = "d6ab15dedfb4aacb18e54fa7c6923195"
     city = "Warszawa"
     url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
@@ -25,6 +25,23 @@ def get_weather_descprition_and_temp():
             'temp_max': temp_max,
             'pressure': pressure}
 
+def weather_desc():
+    weather_dict = get_weather_descprition_and_temp()
+    today = date.today()
+    current_date = today.strftime("%d.%m.%Y")
+
+    Today_date = 'Today is: ' + current_date
+    Today_forecast = 'Today forecast is:' + weather_dict.get('description')
+    Today_minumum_temperature = 'The minimum temperature is',  weather_dict.get('temp_min'), 'Celcius degrees.'
+    Today_maximum_temperature = 'The maximum temperature is',  weather_dict.get('temp_max'), 'Celcius degrees.'
+    Today_pressure = 'The pressure is ', weather_dict.get('pressure'), 'Hectopascals.'
+
+    Today_weather = Today_date, Today_forecast, Today_minumum_temperature, Today_maximum_temperature, Today_pressure
+
+    f = open("weather.txt", "w")
+    f.write(str(Today_weather))
+    f.close()
+
 def main():
     weather_dict = get_weather_descprition_and_temp()
     today = date.today()
@@ -32,7 +49,7 @@ def main():
 
     # Print the results
     print("Today is:", current_date)
-    print("Today`s forecast is",  weather_dict.get('description'))
+    print("Today forecast is",  weather_dict.get('description'))
     print("The minimum temperature is", weather_dict.get('temp_min'), "Celcius degrees.")
     print("The maximum temperature is", weather_dict.get('temp_max'), "Celcius degrees.")
     print("The pressure is", weather_dict.get('pressure'), "Hectopascals.")
@@ -41,12 +58,14 @@ def main():
     filename = 'weather'
 
     data = {}
-    data['Date'] = str(current_date)
-    data['Today`s forecast is '] = weather_dict.get('description')
+    data['Date'] = (current_date)
+    data['Current forecast is '] = weather_dict.get('description')
     data['The minimum temperature is '] = weather_dict.get('temp_min')
     data['The maximum temperature is '] = weather_dict.get('temp_max')
     data['The pressure is '] = weather_dict.get('pressure')
 
     writeToJSONFile(path, filename, data)
+
+    weather_desc()
 
 main()
