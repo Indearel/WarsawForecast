be653dabae779adb0d2ae59bcff9398c1f042ef8
cyberindygos <cyberindygos@gmail.com>
2022-06-24 07:55:25 +0200

    Formatting txt file + Celsius unit name changed.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def writeToJSONFile(path, fileName, data):
         json.dump(data, fp)
 
 def get_weather_descprition_and_temp():
-    api_key = "d6ab15dedfb4aacb18e54fa7c6923195"
+    api_key = "token"
     city = "Warszawa"
     url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
