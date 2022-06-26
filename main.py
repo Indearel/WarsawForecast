861e9cbf7ce31d4d2af315fbcaba161a0300b968
cyberindygos <cyberindygos@gmail.com>
2022-06-26 15:04:53 +0200

    Refactoring, added separated print fuinction

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from datetime import date
-from functions import weather_api
+from functions import weather_print
 from functions import weather_json
 from functions import weather_txt
 
 
 def main():
-    weather_dict = weather_api.get_weather_descprition_and_temp()
-    today = date.today()
-    current_date = today.strftime("%d.%m.%Y")
 
-    # Print the results
-    print("Today is:", current_date)
-    print("Today forecast is", weather_dict.get('description'))
-    print("The minimum temperature is", weather_dict.get('temp_min'), "Celsius degrees.")
-    print("The maximum temperature is", weather_dict.get('temp_max'), "Celsius degrees.")
-    print("The pressure is", weather_dict.get('pressure'), "Hectopascals.")
-
+    weather_print.weather_print_terminal()
     weather_json.weather_desc_json()
     weather_txt.weather_desc_txt()
 
