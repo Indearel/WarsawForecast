1a0bbbc76aadb1708aff7fe825f806a80916503d
cyberindygos <cyberindygos@gmail.com>
2022-07-13 21:30:38 +0200

    Refactoring save to csv

--- functions/weather_csv.py
+++ functions/weather_csv.py
@@ -11,10 +11,10 @@ def weather_desc_csv():
 
     today_date = 'Today is: ' + current_date + ' '
     today_forecast = 'Today forecast is:' + weather_dict.get('description') + '. '
-    today_minimum_temperature = 'The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
-    today_maximum_temperature = 'The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
+    today_min_temp = 'The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
+    today_max_temp = 'The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
     today_pressure = 'The pressure is ' + str(weather_dict.get('pressure')) + ' Hectopascals.'
 
-    weather_csv = open('weather.csv', 'w', newline='')
+    weather_csv = open('weather.csv', 'a', newline='')
     weather_csv_write = csv.writer(weather_csv)
-    weather_csv_write.writerow([today_date, today_forecast, today_minimum_temperature, today_maximum_temperature, today_pressure])
+    weather_csv_write.writerow([today_date, today_forecast, today_min_temp, today_max_temp, today_pressure])
