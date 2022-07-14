b0376574099ecd48a8b9867a9d19e80381575cba
cyberindygos <cyberindygos@gmail.com>
2022-07-14 22:14:10 +0200

    Added current time to csv + small refactor

--- functions/weather_csv.py
+++ functions/weather_csv.py
@@ -1,20 +1,23 @@
 import csv
 
 from datetime import date
+from datetime import datetime
 from functions import weather_api
 
 
 def weather_desc_csv():
     weather_dict = weather_api.get_weather_descprition_and_temp()
     today = date.today()
+    now = datetime.now()
     current_date = today.strftime("%d.%m.%Y")
+    current_time = now.strftime("%H:%M:%S")
 
     today_date = 'Today is: ' + current_date + ' '
-    today_forecast = 'Today forecast is:' + weather_dict.get('description') + '. '
-    today_min_temp = 'The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
-    today_max_temp = 'The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
-    today_pressure = 'The pressure is ' + str(weather_dict.get('pressure')) + ' Hectopascals.'
+    today_forecast = ' Forecast is:' + weather_dict.get('description') + '. '
+    today_min_temp = ' The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
+    today_max_temp = ' The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
+    today_pressure = ' The pressure is ' + str(weather_dict.get('pressure')) + ' Hectopascals.'
 
     weather_csv = open('weather.csv', 'a', newline='')
     weather_csv_write = csv.writer(weather_csv)
-    weather_csv_write.writerow([today_date, today_forecast, today_min_temp, today_max_temp, today_pressure])
+    weather_csv_write.writerow([today_date + current_time , today_forecast, today_min_temp, today_max_temp, today_pressure])
--- functions/weather_print.py
+++ functions/weather_print.py
@@ -1,12 +1,15 @@
 from datetime import date
+from datetime import datetime
 from functions import weather_api
 
 def weather_print_terminal():
     weather_dict = weather_api.get_weather_descprition_and_temp()
     today = date.today()
+    now = datetime.now()
     current_date = today.strftime("%d.%m.%Y")
+    current_time = now.strftime("%H:%M:%S")
 
-    print("Today is:", current_date)
+    print("Today is:", current_date, current_time)
     print("Today forecast is", weather_dict.get('description'))
     print("The minimum temperature is", weather_dict.get('temp_min'), "Celsius degrees.")
     print("The maximum temperature is", weather_dict.get('temp_max'), "Celsius degrees.")
