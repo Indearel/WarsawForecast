42eb74098bef423be9acdfc63c8dd6a12b4c6eaf
cyberindygos <cyberindygos@gmail.com>
2022-07-18 20:37:51 +0200

    Small csv refactor

--- functions/weather_csv.py
+++ functions/weather_csv.py
@@ -6,7 +6,7 @@ from functions import weather_api
 
 
 def weather_desc_csv():
-    weather_dict = weather_api.get_weather_descprition_and_temp()
+    weather_dict = weather_api.get_weather_description_and_temp()
     today = date.today()
     current_date = today.strftime("%d.%m.%Y")
     now = datetime.now()
@@ -17,7 +17,8 @@ def weather_desc_csv():
     today_min_temp = ' The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
     today_max_temp = ' The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
     today_pressure = ' The pressure is ' + str(weather_dict.get('pressure')) + ' Hectopascals.'
+    today_all = [today_date + current_time, today_forecast, today_min_temp, today_max_temp, today_pressure]
 
     weather_csv = open('weather.csv', 'a', newline='')
     weather_csv_write = csv.writer(weather_csv)
-    weather_csv_write.writerow([today_date + current_time, today_forecast, today_min_temp, today_max_temp, today_pressure])
+    weather_csv_write.writerow(today_all)
