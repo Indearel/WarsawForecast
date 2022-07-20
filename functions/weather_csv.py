805768232f77ee57f439bb1baa06c3cc466478cc
cyberindygos <cyberindygos@gmail.com>
2022-07-20 19:23:12 +0200

    Small teaks

--- functions/weather_csv.py
+++ functions/weather_csv.py
@@ -13,9 +13,9 @@ def weather_desc_csv():
     current_time = now.strftime("%H:%M:%S")
 
     today_date = 'Today is: ' + current_date + ' '
-    today_forecast = ' Forecast is:' + weather_dict.get('description') + '. '
-    today_min_temp = ' The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees. '
-    today_max_temp = ' The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees. '
+    today_forecast = ' Forecast is:' + weather_dict.get('description') + '.'
+    today_min_temp = ' The minimum temperature is ' + str(weather_dict.get('temp_min')) + ' Celsius degrees.'
+    today_max_temp = ' The maximum temperature is ' + str(weather_dict.get('temp_max')) + ' Celsius degrees.'
     today_pressure = ' The pressure is ' + str(weather_dict.get('pressure')) + ' Hectopascals.'
     today_all = [today_date + current_time, today_forecast, today_min_temp, today_max_temp, today_pressure]
 
