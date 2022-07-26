f8755cabc03ea37c2badab28f29bc34f25546fb2
cyberindygos <cyberindygos@gmail.com>
2022-07-26 21:28:42 +0200

    Small tweaks + data update

--- functions/weather_json.py
+++ functions/weather_json.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from functions import weather_api
 
 
-def write_to_json_file(path, fileName, data):
-    file_path_name_w_ext = './' + path + '/' + fileName + '.json'
+def write_to_json_file(path, filename, data):
+    file_path_name_w_ext = './' + path + '/' + filename + '.json'
     with open(file_path_name_w_ext, 'a') as fp:
         json.dump(data, fp)
 
@@ -20,8 +20,7 @@ def weather_desc_json():
     filename = 'weather'
 
     data = {}
-    data['Date'] = current_date
-    data['Time'] = current_time
+    data['Date'] = current_date + current_time
     data['Current forecast is '] = weather_dict.get('description')
     data['The minimum temperature is '] = weather_dict.get('temp_min')
     data['The maximum temperature is '] = weather_dict.get('temp_max')
